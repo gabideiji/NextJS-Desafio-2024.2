8e2c440ee0687900b80e42562adac849d360dd17
Gabriella <gabriellamargarida18@gmail.com>
2024-08-25 20:14:37 -0300

    inicia

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ['300', '400', '500', '600', '700', '800'], 
+  subsets: ["latin"], 
+});
 
 export const metadata: Metadata = {
-  title: "Desafio Next.JS 2024.2",
-  description: "Desafio Next.JS, Typescript, Tailinwd, Prisma, semestre 2024.2 para todos os membros da Code JR",
+  title: "Kitty Shop",
+  description: "Produtos da marca Sanrio",
 };
 
 export default function RootLayout({
@@ -16,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <header />
+          {children}
+          </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
+import Header from "../../components/search/header";
 
 export default function Home() {
   return (
-    <></>
+    <Header />
+    
   );
 }
