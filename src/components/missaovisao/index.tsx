cea1d3152a8935010d03dad90632ff63c537d733
Gabriella <gabriellamargarida18@gmail.com>
2024-09-07 15:28:04 -0300

    acaba api

--- src/components/missaovisao/index.tsx
+++ src/components/missaovisao/index.tsx
@@ -1,30 +1,41 @@
-export default function MissaoVisao() {
+'use client';
+import axios from 'axios';
+import getApi from '@/api';
+import { useEffect, useState } from 'react';
+
+type ApiProps = {
+    id: number;
+    title: string;
+    text: string;
+};
+
+export default function ApiComponent() {
+    const [cards, setCards] = useState<ApiProps[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const { identities } = await getApi();
+                setCards(identities);
+            } catch (error) {
+                console.error('Erro ao carregar os dados:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
-        
-        <div className="flex items-center justify-center w-full h-screen bg-[#FFD1D1] p-8 overflow-hidden rounded-lg">
-            <div className="flex gap-8 w-full h-full">
-                
-                <div className="bg-[#D9D9D9] flex-1 p-8 rounded-lg w-1/3">
-                    <h2 className="text-3xl font-bold mb-4 text-white">Missão</h2>
-                    <p className="text-lg text-white break-words">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar enim vel est finibus, vel tempus ligula volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec vel venenatis nisi. Proin sit amet urna quam. Etiam iaculis nulla risus, ac finibus sapien tincidunt a. Integer purus tortor,
-                    </p>
-                </div>
-
-                <div className="bg-gray-300 flex-1 p-8 rounded-lg w-1/3">
-                    <h2 className="text-3xl font-bold mb-4 text-white">Visão</h2>
-                    <p className="text-lg text-white break-words">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar enim vel est finibus, vel tempus ligula volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec vel venenatis nisi. Proin sit amet urna quam. Etiam iaculis nulla risus, ac finibus sapien tincidunt a. Integer purus tortor,
-                    </p>
-                </div>
-
-                <div className="bg-gray-300 flex-1 p-8 rounded-lg w-1/3">
-                    <h2 className="text-3xl font-bold mb-4 text-white">Valores</h2>
-                    <p className="text-lg text-white break-words ">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar enim vel est finibus, vel tempus ligula volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec vel venenatis nisi. Proin sit amet urna quam. Etiam iaculis nulla risus, ac finibus sapien tincidunt a. Integer purus tortor,
-
-                    </p>
-                </div>
+        <div className="flex items-center justify-center w-full bg-[#FFD1D1] p-8 overflow-hidden rounded-lg">
+            <div className="flex flex-col md:flex-row gap-8 w-full">
+                {cards.map((card) => {
+                    return (
+                        <div key={card.id} className={`flex-1 p-8 rounded-lg md:w-1/3 bg-[#7e7e7e]`}>
+                            <h3 className="text-3xl font-bold mb-4 text-white">{card.title}</h3>
+                            <p className="text-lg text-white break-words">{card.text}</p>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
