c7ff5eaeb87e61c281dd09f64cd2923aeed4831d
Gabriella <gabriellamargarida18@gmail.com>
2024-09-09 17:45:57 -0300

    busca por personagem

--- src/components/personagens/bonecas/index.tsx
+++ src/components/personagens/bonecas/index.tsx
@@ -1,25 +1,34 @@
-import Image from "next/image"; 
-import { StaticImageData } from "next/image";
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 type CardsPersonagensProps = {
-    title: string;
-        src: string;
-}
+  title: string;
+  src: string;
+};
 
+export default function CardsPersonagens({ title, src }: CardsPersonagensProps) {
+  const router = useRouter();
 
-export default function CardsPersonagens({title, src}:CardsPersonagensProps){
-    return(
-        <div className="flex flex-col items-center justify-center gap-8 max-w-sm 2xl:max-w-md 3xl:max-w-lg p-8
-        transition-shadow duration-200 rounded-md hover:shadow-2xl border border-pink-300">
-            <Image 
-            src={src}
-            alt={title}
-            width= {200}
-            height= {120}
-            className="w-[200px] h-[200px] object-cover rounded-md"
-            />
-            <h1 className="text-center text-xl xl:text-2xl 2xl:text-3xl 3xl:text-4x text-pink-950 
-            font-semibold cursor-default">{title}</h1>
-        </div>
-    )
+  const handleClick = () => {
+    router.push(`/pesquisa/?query=${title.toLowerCase()}`);
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      className="flex flex-col items-center justify-center gap-8 max-w-sm 2xl:max-w-md 3xl:max-w-lg p-8 transition-shadow duration-200 rounded-md hover:shadow-2xl border border-pink-300 cursor-pointer"
+    >
+      <Image
+        src={src}
+        alt={title}
+        width={200}
+        height={120}
+        className="w-[200px] h-[200px] object-cover rounded-md"
+      />
+      <h1 className="text-center text-xl xl:text-2xl 2xl:text-3xl 3xl:text-4x text-pink-950 font-semibold cursor-default">
+        {title}
+      </h1>
+    </div>
+  );
 }
